399fa5a3a1d079e409d90f052184c97f3e77e2c9
daniel107x-hub <dmejiav310796@outlook.com>
2024-11-07 13:36:36 -0600

    Added concurrency support to simple redis server

---
+++ RedisClone/AsyncRedisServer.cs
@@ -0,0 +1,130 @@
+using System.Net;
+using System.Net.Sockets;
+using RedisClone.Services.Serialization;
+
+namespace RedisClone;
+
+public class AsyncRedisServer
+{
+    private Dictionary<string, object> _dictionary;
+    private readonly TcpListener _tcpListener;
+    private const int Port = 6379;
+    private int _clients;
+    private int _totalSetRequests;
+    private int _totalGetRequests;
+    private int _totalConfigRequests;
+    private int _totalUnrecognizedRequests;
+    
+    public AsyncRedisServer()
+    {
+        _tcpListener = new TcpListener(IPAddress.Any, Port);
+        _dictionary = new();
+    }
+
+    public async Task StartAsync()
+    {
+        try
+        {
+            _tcpListener.Start();
+            Console.WriteLine($"Server started on port {Port}");
+            while (true)
+            {
+                TcpClient client = await _tcpListener.AcceptTcpClientAsync();
+                Console.WriteLine("Client connected");
+                _clients++;
+                _ = HandleClientAsync(client);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+        finally
+        {
+            _tcpListener.Stop();
+        }
+    }
+
+    private async Task HandleClientAsync(TcpClient client)
+    {
+        using NetworkStream stream = client.GetStream();
+        try
+        {
+            // Receive request
+            StreamReader streamReader = new(stream);
+            while (streamReader.Peek() != -1)
+            {
+                // Process request
+                Console.WriteLine("New request received");
+                byte[] response = await Task.Run(() => ProcessRequest(streamReader));
+                // Send response
+                stream.Write(response, 0, response.Length);
+                Console.WriteLine("Response sent");
+            }
+            Console.WriteLine(PrintMetrics());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+        finally
+        {
+            stream.Close();
+            client.Close();
+        }
+    }
+
+    private byte[] ProcessRequest(StreamReader streamReader)
+    {
+        char type = (char)streamReader.Read();
+        ISerializationService deSerializationService = SerializationServiceFactory.GetSerializationService(type);
+        List<object> commands = (List<object>)deSerializationService.Deserialize(streamReader);
+        string command = (string) commands[0];
+        object[] args = commands.Skip(1).ToArray();
+            
+        // Handle request
+        object response;
+        if (command.Equals("PING"))
+        {
+            response = "PONG";
+        }else if (command.Equals("ECHO"))
+        {
+            response = args[0];
+        }else if (command.Equals("SET"))
+        {
+            Interlocked.Increment(ref _totalSetRequests);
+            string key = (string)args[0];
+            object value = args[1];
+            _dictionary[key] = value;
+            response = "OK";
+        }else if (command.Equals("GET"))
+        {
+            Interlocked.Increment(ref _totalGetRequests);
+            string key = (string)args[0];
+            _dictionary.TryGetValue(key, out object value);
+            response = value;
+        }else if (command.Equals("RESET"))
+        {
+            _clients = 0;
+            _dictionary.Clear();
+            Interlocked.Exchange(ref _totalSetRequests, 0);
+            Interlocked.Exchange(ref _totalGetRequests, 0);
+            Interlocked.Exchange(ref _totalConfigRequests, 0);
+            Interlocked.Exchange(ref _totalUnrecognizedRequests, 0);
+            response = "OK";
+        }
+        else
+        {
+            if(command.Equals("CONFIG")) Interlocked.Increment(ref _totalConfigRequests);
+            else Interlocked.Increment(ref _totalUnrecognizedRequests);
+            response = "";
+        }
+        ISerializationService serializationService = SerializationServiceFactory.GetSerializationService(response);
+        return serializationService.Serialize(response);
+    }
+    
+    private string PrintMetrics()
+    {
+        return $"Clients: {_clients}, Total SET requests: {_totalSetRequests}, Total GET requests: {_totalGetRequests}, Total CONFIG requests: {_totalConfigRequests}, Total unrecognized requests: {_totalUnrecognizedRequests}";
+    }
+}
--- RedisClone/Program.cs
+++ RedisClone/Program.cs
@@ -1,90 +1,11 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using RedisClone.Services.Serialization;
-
-namespace RedisClone
+﻿namespace RedisClone
 {
     class Program
     {
-        private static Dictionary<string, object> dictionary = new();
         static void Main(string[] args)
         {
-            IPAddress address = IPAddress.Any;
-            int port = 6379;
-            TcpListener listener = new(address, port);
-            try
-            {
-                listener.Start();
-                Console.WriteLine($"Server started on port {port}");
-                while (true)
-                {
-                    TcpClient client = listener.AcceptTcpClient();
-                    Console.WriteLine("Client connected");
-                    HandleClient(client);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error: {e.Message}");
-            }
-            finally
-            {
-                listener.Stop();
-            }
-        }
-
-        private static void HandleClient(TcpClient client)
-        {
-            NetworkStream stream = client.GetStream();
-            try
-            {
-                // Receive request
-                StreamReader streamReader = new(stream);
-                char type = (char)streamReader.Read();
-                ISerializationService deSerializationService = SerializationServiceFactory.GetSerializationService(type);
-                List<object> commands = (List<object>)deSerializationService.Deserialize(streamReader);
-                string command = (string) commands[0];
-                object[] args = commands.Skip(1).ToArray();
-                
-                // Handle request
-                object response = null;
-                if (command.Equals("PING"))
-                {
-                    response = "PONG";
-                }else if (command.Equals("ECHO"))
-                {
-                    response = args[0];
-                }else if (command.Equals("SET"))
-                {
-                    string key = (string)args[0];
-                    object value = args[1];
-                    dictionary[key] = value;
-                    response = "OK";
-                }else if (command.Equals("GET"))
-                {
-                    string key = (string)args[0];
-                    dictionary.TryGetValue(key, out object value);
-                    response = value;
-                }
-                else
-                {
-                    response = "";
-                }
-                // Send response
-                ISerializationService serializationService = SerializationServiceFactory.GetSerializationService(response);
-                byte[] responseData = serializationService.Serialize(response);
-                stream.Write(responseData, 0, responseData.Length);
-                Console.WriteLine("Response sent");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error: {e.Message}");
-            }
-            finally
-            {
-                stream.Close();
-                client.Close();
-            }
+            AsyncRedisServer server = new();
+            server.StartAsync().Wait();
         }
     }
 }
