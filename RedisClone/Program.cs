360bf765c4700a015aaa2a563f36dc89099adcd4
daniel107x-hub <dmejiav310796@outlook.com>
2024-11-07 11:18:17 -0600

    Added basic server and PING and ECHO commands

--- RedisClone/Program.cs
+++ RedisClone/Program.cs
@@ -1,9 +1,75 @@
-﻿namespace RedisClone
+﻿using System.Net;
+using System.Net.Sockets;
+using RedisClone.Services.Serialization;
+
+namespace RedisClone
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IPAddress address = IPAddress.Any;
+            int port = 6379;
+            TcpListener listener = new(address, port);
+            try
+            {
+                listener.Start();
+                Console.WriteLine($"Server started on port {port}");
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Client connected");
+                    HandleClient(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+
+        private static void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            try
+            {
+                // Receive request
+                StreamReader streamReader = new(stream);
+                char type = (char)streamReader.Read();
+                ISerializationService deSerializationService = SerializationServiceFactory.GetSerializationService(type);
+                List<object> commands = (List<object>)deSerializationService.Deserialize(streamReader);
+                string command = (string) commands[0];
+                object[] args = commands.Skip(1).ToArray();
+                
+                // Handle request
+                object response = null;
+                if (command.Equals("PING"))
+                {
+                    response = "PONG";
+                }else if (command.Equals("ECHO"))
+                {
+                    response = args[0];
+                }
+                
+                // Send response
+                ISerializationService serializationService = SerializationServiceFactory.GetSerializationService(response);
+                byte[] responseData = serializationService.Serialize(response);
+                stream.Write(responseData, 0, responseData.Length);
+                Console.WriteLine("Response sent");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
         }
     }
 }
